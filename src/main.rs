744b99ec0d94bef1e7072280c82fe702de6cad63
aspect19 <jeffamenya388@gmail.com>
2025-06-14 09:55:43 +0300

    Add a settings command

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,35 @@
-use teloxide::prelude::*;
+use teloxide::{prelude::*,  utils::command::BotCommands};
 use dotenvy::dotenv;
 
 #[tokio::main]
 async fn main() {
     dotenv().ok();
-    
+
     pretty_env_logger::init();
     log::info!("Starting throw dice bot...");
 
     let bot = Bot::from_env();
 
-    teloxide::repl(bot, |bot: Bot, msg: Message| async move {
-        bot.send_dice(msg.chat.id).await?;
-        Ok(())
-    })
-    .await;
+    Command::repl(bot, answer).await;
+
+}
+
+#[derive(BotCommands, Clone)]
+#[command(rename_rule = "lowercase", description = "These commands are supported:")]
+enum Command {
+    #[command(description = "display this text.")]
+    Help,
+    #[command(description = "handle a username.")]
+    Settings,
+}
+
+async fn answer(bot: Bot, msg: Message, cmd: Command) -> ResponseResult<()> {
+    match cmd {
+        Command::Help => bot.send_message(msg.chat.id, Command::descriptions().to_string()).await?,
+        Command::Settings => {
+            bot.send_message(msg.chat.id, format!("Choose a video quality to use")).await?
+        }
+    };
+
+    Ok(())
 }

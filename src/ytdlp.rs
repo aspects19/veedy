6f77edd93ac4e9379dd5ab7fd4e8ba5573ad8c05
Amenya <133534815+aspects19@users.noreply.github.com>
2025-12-07 12:28:31 +0300

    Detect or install ytdlp

--- src/ytdlp.rs
+++ src/ytdlp.rs
@@ -1,4 +1,3 @@
-use std::{fs, path::{Path, PathBuf}, process::Command};
 use std::process::Command;
 use std::path::Path;
 use std::os::unix::fs::PermissionsExt; //TODO: Solve for Windows
@@ -8,32 +7,35 @@ use tokio::fs;
 pub async fn prepare_ytdlp() -> Result<(), Box<dyn std::error::Error>> {
     if Command::new("yt-dlp").arg("--version").output().is_ok() {
         println!("System yt-dlp Found. Skipping download.");
-        return;
+        return Ok(());
     }
 
     if Path::new("lib/yt-dlp").exists() {
         println!("Local yt-dlp found in lib/.");
-        return;
+        return Ok(());
     }
 
     println!("yt-dlp missing. Downloading standalone binary...");
-    download_ytdlp();
+    download_ytdlp().await?;
+    Ok(())
 }
 
 async fn download_ytdlp() -> Result<(), Box<dyn std::error::Error>> {
-    fs::create_dir_all("lib").unwrap(); 
+    fs::create_dir_all("lib").await?; 
 
     // TODO: Dynamify this for different environments/OS (Windows?)
     let url = "https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp_linux";
     let dest_path = "lib/yt-dlp";
 
     let resp = reqwest::get(url).await?;
-    fs::write(dest_path, resp.bytes().await?);
+    fs::write(dest_path, resp.bytes().await?).await?;
 
     // TODO: handle other Environments
-    let mut perms = fs::metadata(dest_path).unwrap().permissions();
+    let mut perms = fs::metadata(dest_path).await?.permissions();
     perms.set_mode(0o755); // rwxr-xr-x
-    fs::set_permissions(dest_path, perms).unwrap();
+    fs::set_permissions(dest_path, perms).await?;
 
     println!("yt-dlp is ready");
+
+    Ok(())
 }

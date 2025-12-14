27feb91bb60b69ca89135e92cb510793743e83c6
Amenya <133534815+aspects19@users.noreply.github.com>
2025-12-14 18:54:55 +0300

    Using separate dptree brances

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,25 @@
+pub mod ffmpeg;
 pub mod inlinekeyboard;
 pub mod inlinequery;
 pub mod youtubeurl;
 pub mod ytdlp;
-pub mod ffmpeg;
 
+use crate::inlinequery::{handle_chosen_inline_result, handle_inline_query};
 use dotenvy::dotenv;
 use teloxide::prelude::*;
-use crate::inlinequery::{handle_chosen_inline_result, handle_inline_query};
 
 #[tokio::main]
 async fn main() {
     dotenv().ok();
     pretty_env_logger::init();
 
-
-    let bot = Bot::from_env(); 
+    let bot = Bot::from_env();
 
     let handler = dptree::entry()
-        .branch(
-            Update::filter_inline_query()
-            .endpoint(handle_inline_query)
-        )
-        
-        .branch(
-            Update::filter_chosen_inline_result()
-            .endpoint( handle_chosen_inline_result )
-        );
+        .branch(Update::filter_inline_query().endpoint(handle_inline_query))
+        .branch(Update::filter_chosen_inline_result().endpoint(handle_chosen_inline_result));
 
-    Dispatcher::builder( bot, handler )
+    Dispatcher::builder(bot, handler)
         .enable_ctrlc_handler()
         .build()
         .dispatch()

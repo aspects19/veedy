a89f25a3602084942d5a15e8ded3d3d439ef6aaa
Amenya <133534815+aspects19@users.noreply.github.com>
2025-12-14 18:53:21 +0300

    Inline keyboard utility

--- src/inlinekeyboard.rs
+++ src/inlinekeyboard.rs
@@ -2,7 +2,7 @@ use teloxide::types::{InlineKeyboardButton, InlineKeyboardMarkup};
 
 #[allow(unused)]
 pub async fn keyboardbtns() -> InlineKeyboardMarkup {
-       let keyboard = InlineKeyboardMarkup::new(vec![
+    let keyboard = InlineKeyboardMarkup::new(vec![
         vec![
             InlineKeyboardButton::callback("4K", "4K"),
             InlineKeyboardButton::callback("1080P", "1080P"),
@@ -13,8 +13,8 @@ pub async fn keyboardbtns() -> InlineKeyboardMarkup {
             InlineKeyboardButton::callback("360P", "360P"),
             InlineKeyboardButton::callback("256K", "256K"),
             InlineKeyboardButton::callback("128K", "128K"),
-        ]
+        ],
     ]);
-    
+
     keyboard
 }
